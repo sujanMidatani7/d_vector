5f4c9fc81cadd9525ec935293cbe11c1c9c32eb2
Sujan midatani <105052933+sujanMidatani7@users.noreply.github.com>
2023-04-26 23:55:26 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,36 +1,36 @@
 import streamlit as st
 import torch
-import torchaudio
-from sklearn.decomposition import PCA
+
+
 from sklearn.metrics.pairwise import cosine_similarity
 import librosa
 import numpy as np
 
-# Load the encoder classifier model
-# def compute_similarity(x1, x2):
-#     x1_tensor = torch.tensor(x1)
-#     x2_tensor = torch.tensor(x2)
-#     dot_product = torch.sum(x1_tensor * x2_tensor, dim=-1)
-#     norm_x1 = torch.norm(x1_tensor, dim=-1)
-#     norm_x2 = torch.norm(x2_tensor, dim=-1)
-#     cosine_similarity = dot_product / (norm_x1 * norm_x2)
-#     return cosine_similarity
-def extract_mfcc_features(audio_file):
-    # Load the audio file
-    y, sr = librosa.load(audio_file,sr=16000)
-#     y=y.numpy()
-    # Compute the MFCC features
-    mfcc = librosa.feature.mfcc(y=y, sr=sr, n_mfcc=360)
-    mfcc_delta = librosa.feature.delta(mfcc)
-    mfcc_delta2 = librosa.feature.delta(mfcc, order=2)
-    mfcc_features = np.concatenate((mfcc, mfcc_delta, mfcc_delta2), axis=0)
-    st.write(mfcc_features.shape)
-    n_features, n_samples = mfcc_features.shape
-    mfcc_features = mfcc_features.reshape(n_features, -1)
-    mfcc_features = mfcc_features.reshape(n_features*n_samples, 2)
+# # Load the encoder classifier model
+# # def compute_similarity(x1, x2):
+# #     x1_tensor = torch.tensor(x1)
+# #     x2_tensor = torch.tensor(x2)
+# #     dot_product = torch.sum(x1_tensor * x2_tensor, dim=-1)
+# #     norm_x1 = torch.norm(x1_tensor, dim=-1)
+# #     norm_x2 = torch.norm(x2_tensor, dim=-1)
+# #     cosine_similarity = dot_product / (norm_x1 * norm_x2)z
+# #     return cosine_similarity
+# def extract_mfcc_features(audio_file):
+#     # Load the audio file
+#     y, sr = librosa.load(audio_file,sr=16000)
+# #     y=y.numpy()
+#     # Compute the MFCC features
+#     mfcc = librosa.feature.mfcc(y=y, sr=sr, n_mfcc=360)
+#     mfcc_delta = librosa.feature.delta(mfcc)
+#     mfcc_delta2 = librosa.feature.delta(mfcc, order=2)
+#     mfcc_features = np.concatenate((mfcc, mfcc_delta, mfcc_delta2), axis=0)
+#     st.write(mfcc_features.shape)
+#     n_features, n_samples = mfcc_features.shape
+#     mfcc_features = mfcc_features.reshape(n_features, -1)
+#     mfcc_features = mfcc_features.reshape(n_features*n_samples, 2)
 
 
-    return mfcc_features
+#     return mfcc_features
 
 # def load_audio(file):
 #     # Load audio file using Torchaudio
@@ -39,38 +39,50 @@ def extract_mfcc_features(audio_file):
 #         return waveform, sample_rate
     
 
-def compute_dvector(mfcc_features):
+def compute_dvector(audio_file):
     # Apply PCA to reduce the dimensionality of the MFCC features
 #     mfcc_features=mfcc_features.reshape(-1,1)
-    st.write(mfcc_features.shape)
-    pca = PCA(n_components=120)
-    pca.fit(mfcc_features)
-    mfcc_pca = pca.transform(mfcc_features)
+#     st.write(mfcc_features.shape)
+    audio, sr = librosa.load(audio_file, sr=16000)
+    frame_length = int(sr * 0.025)  # 25 ms
+    hop_length = int(sr * 0.010)  # 10 ms
+    n_fft = 512
+    n_mels = 40
+    n_components = 10
 
-    # Compute the mean and standard deviation of the PCA-transformed MFCC features
-    mfcc_pca_mean = np.mean(mfcc_pca, axis=0)
-    mfcc_pca_std = np.std(mfcc_pca, axis=0)
+    frame_length2 = int(sr2 * 0.025)  # 25 ms
+    hop_length2 = int(sr2 * 0.010)  # 10 ms
+    n_fft2 = 512
+    n_mels2 = 40
+    n_components2 = 10
 
-    # Normalize the PCA-transformed MFCC features
-    mfcc_pca_norm = (mfcc_pca - mfcc_pca_mean) / mfcc_pca_std
+    S = librosa.feature.melspectrogram(y=audio, sr=sr, n_fft=n_fft,
+                                    hop_length=hop_length, n_mels=n_mels)
 
     # Compute the d-vector by averaging the normalized PCA-transformed MFCC features
-    dvector = np.mean(mfcc_pca_norm, axis=0)
+    S = np.log(S + 1e-9)
 
-    return dvector
+# compute delta and delta-delta
+    delta = librosa.feature.delta(S, width=3)
+    delta_delta = librosa.feature.delta(S, order=2, width=3)
+
+# concatenate features
+    features = np.concatenate((S, delta, delta_delta), axis=0)
+    mean = np.mean(features, axis=1)
+    cov = np.cov(features)
+    
+    U, s, Vh = np.linalg.svd(cov)
+    A = np.dot(np.dot(U, np.diag(np.sqrt(s))), U.T)
+    x = np.mean(features, axis=1)
+# d = np.dot(np.dot(A, U.T), (x - mean))
+    d=np.dot(A, U.T)
+    return d
 def compute_similarity(dvector1, dvector2):
     # Compute the cosine similarity between the d-vectors
-    similarity = cosine_similarity(dvector1, dvector2)[0][0]
+    similarity = cosine_similarity([dvector1], [dvector2])[0][0]
 
     return similarity
-def softmax(z):
-    """Computes softmax function.
-    z: array of input values.
-    Returns an array of outputs with the same shape as z."""
-    # For numerical stability: make the maximum of z's to be 0.
-    shiftz = z - np.max(z)
-    exps = np.exp(shiftz)
-    return exps / np.sum(exps)
+
 
 # Define Streamlit app
 st.title("Audio Analysis")
@@ -78,25 +90,23 @@ st.write("Comparision of two audio samples using xvectors.")
 
 # Create audio input component
 audio_file1 = st.file_uploader("Choose 1st audio  file", type=["mp3", "wav", "flac"])
-mfcc_features1=torch.rand(2, 3,1)
-mfcc_features2=torch.rand(2, 3,1)
-# Analyze audio properties when file is uploaded
+
 dvector1=torch.rand(2, 3,1)
 dvector2=torch.rand(2, 3,1)
 if audio_file1 is not None:
-    mfcc_features1 = extract_mfcc_features(audio_file1)
-    dvector1 = compute_dvector(mfcc_features1)
+    
+    dvector1 = compute_dvector(audio_file1)
 
 audio_file2=st.file_uploader("Choose 2nd audio  file", type=["mp3", "wav", "flac"])
 if audio_file2 is not None:
-    mfcc_features2 = extract_mfcc_features(audio_file2)
-    dvector2 = compute_dvector(mfcc_features2)
+#     mfcc_features2 = extract_mfcc_features(audio_file2)
+    dvector2 = compute_dvector(audio_file2)
 
 st.write("the similarity of the given two audio files is:")
 similarity = compute_similarity(dvector1, dvector2)
 st.write(similarity)
 try:
-    if similarity>=0:
+    if similarity>=0.9:
         st.write("both are equal")
     else :
         st.write("both are not equal")
