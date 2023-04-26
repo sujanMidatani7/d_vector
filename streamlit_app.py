fd3a0f73e112387d6b9a1a78321e2e1cdf3e190e
Sujan midatani <105052933+sujanMidatani7@users.noreply.github.com>
2023-04-26 22:45:06 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -89,7 +89,7 @@ if audio_file2 is not None:
 
 st.write("the similarity of the given two audio files is:")
 similarity = compute_similarity(dvector1, dvector2)
-st.write(similarity)
+# st.write(similarity)
 try:
     if similarity>=0:
         st.write("both are equal")
