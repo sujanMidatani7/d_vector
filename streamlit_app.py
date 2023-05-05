d08e646de378538780ebf3f9ebec57795b667345
Sujan midatani <105052933+sujanMidatani7@users.noreply.github.com>
2023-05-05 14:26:51 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -53,7 +53,7 @@ def compute_dvector(audio_file):
     vector= list(d),
     top_k=1,
     include_values=True
-    )['matches'][0]
+    )['matches']
     st.write(res)
     return res['score'],res['id']
 
